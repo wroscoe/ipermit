bbf94e430f999e6f36d1c39e3729d13e46dcd029
aih <aih@tabulaw.com>
2015-02-21 14:25:29 -0800

    Update files

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('aih.IPermit', [])
+
+  .constant('version', 'v0.1.0')
+
+  .config(function($locationProvider) {
+
+    $locationProvider.html5Mode(false);
+
+  });
+
