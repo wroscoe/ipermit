34df56b1d4f253a5ad9f20c0bc5304e1cb18923d
Will Roscoe <wroscoe@gmail.com>
2015-02-21 15:30:48 -0800

    update main.js

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -4,28 +4,22 @@ angular.module('aih.IPermit')
 
   .controller('MainCtrl', function($scope, $http, $location, version) {
 
+    $scope.searched=true; // has the search button been pressed
+    $scope.address="672 Alcatraz Ave., Oakland, CA 94609";
+    $scope.zone='RM-4';s
+
     $scope.$path = $location.path.bind($location);
     $scope.version = version;
 
 
     console.log('this is the MainCtrl');
 
-  $http.get('zones.json')
-       .then(function(res){
-          $scope.zoneProperties = res.data;                
-        });
+    //load the zone data file
+    $http.get('zones.json')
+         .then(function(res){
+            $scope.zoneProperties = res.data;                
+          });
 
-  $scope.searched=true;
-  $scope.address="672 Alcatraz Ave., Oakland, CA 94609";
-  $scope.zone='RM-4';
+
 
   })
-
-
-
-.controller('TodoCtrl', function($scope, $http) {
-  $http.get('todos.json')
-       .then(function(res){
-          $scope.todos = res.data;                
-        });
-});
